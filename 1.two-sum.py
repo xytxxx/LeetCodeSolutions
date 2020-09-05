fb090ef797fb830603e7e3448bd592fddc972e09
Yitong Xiao <yitongxiao@kitchenmate.com>
2020-09-05 14:12:56 -0400

    day 3

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -7,6 +7,17 @@
 # @lc code=start
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
+        from collections import defaultdict
+        dic = defaultdict(list)
+        for i, num in enumerate(nums):
+            dic[num].append(i)
+        for i, a in enumerate(nums):
+            b = target - a
+            if b in dic:
+                if a == b and len(dic[a]) >= 2:
+                    return tuple(dic[a])
+                elif a != b:
+                    return (dic[a][0], dic[b][0])
+        return (0, 0)
 # @lc code=end
 
